658fd983a07fcbbde2d98bfda97f5ecd2cb4a7b6
inpathos <68312245+inpathos@users.noreply.github.com>
2021-06-18 08:15:33 -0300

    Exercício 1 - feito

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/exercicio1/StefanRotenberg.java
@@ -0,0 +1,8 @@
+package uff.ic.lleme.tcc00328.s20211.exercicio.exercicio1;
+
+public class StefanRotenberg {
+
+    public static void main(String[] args) {
+        System.out.println("Alô mundo!!!!!!!!!");
+    }
+}
