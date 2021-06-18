0fcfa91f1b19eb9883a3d5f7b4e1e1fc0bcde4ee
Stefan Rotenberg <stefanrotenberg@gmail.com>
2021-06-18 08:21:14 -0300

    Exercício 1

--- src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/exercicio1/StefanRotenberg.java
+++ src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/exercicio1/StefanRotenberg.java
@@ -1,6 +1,6 @@
 package uff.ic.lleme.tcc00328.s20211.exercicio.exercicio1;
 
-public class LuizAndre {
+public class StefanRotenberg {
 
     public static void main(String[] args) {
         System.out.println("Alô mundo!!!!!!!!!");
