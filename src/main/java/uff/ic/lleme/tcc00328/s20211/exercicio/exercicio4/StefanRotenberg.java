18a4c30e2e55ee0e93734bb4d638312968cfe456
Stefan Rotenberg <stefanrotenberg@gmail.com>
2021-06-24 18:32:31 -0300

    Exercício 4 - pequena correção

--- src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/exercicio4/StefanRotenberg.java
+++ src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/exercicio4/StefanRotenberg.java
@@ -15,7 +15,7 @@ public class StefanRotenberg {
     
     static boolean ehPrimoWhile(int n){
 	int i = 2;
-	while(i < n / 2){
+	while(i <= n / 2){
 	    if(n % i == 0) return false;
 	    i++;
 	}
